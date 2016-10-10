e9de24f4b43f6eb830f3003926089fd0c0bfb022
codetiger7 <codetiger@icloud.com>
2016-10-10 18:46:31 +0200

    scatterplots

--- project_2_data_visualization/chap_15_generating_data/mpl_squares.py
+++ project_2_data_visualization/chap_15_generating_data/mpl_squares.py
@@ -1,7 +1,18 @@
 
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 25]
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 
-plt.plot(squares)
+plt.scatter(x_values, y_values, s=100)
+
+# set chart, title, and label axes
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# set size of tick labels
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+
 plt.show()
